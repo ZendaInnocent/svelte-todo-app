3648777c5972e5398ead74d92f3184e650d3649a
Innocent Zenda <zendainnocent@gmail.com>
2023-12-31 12:37:36 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  let todos = [
+    { title: 'Learn Svelte', completed: true },
+    { title: 'Learn SvelteKit', completed: false },
+  ]
+
+  let todo
+
+  function handleSubmit() {
+    if (todo === '') return
+    todos = [...todos, { title: todo, completed: false }]
+    todo = ''
+  }
+
+  function deleteTodo(todo) {
+    todos = todos.filter((t) => t !== todo)
+  }
+</script>
+
+<h1>Todo List App</h1>
+
+<div>
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Add Todo" bind:value={todo} />
+    <button type="submit">Submit</button>
+  </form>
+</div>
+
+<div>
+  <ul>
+    {#each todos as todo}
+      <li>
+        {todo.title}
+        <span>
+          <button on:click={deleteTodo.bind(this, todo)}> Delete </button>
+        </span>
+      </li>
+    {/each}
+  </ul>
+  <div>
+    <button on:click={() => (todos = [])}> Delete Tasks </button>
+  </div>
+</div>
