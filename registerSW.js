931210ffc6643c007747140939c3f7d258b69fab
Innocent Zenda <zendainnocent@gmail.com>
2024-01-07 12:42:51 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/svelte-todo-app/sw.js', { scope: '/svelte-todo-app/' })})}
